19313e4e5f7c74a03b48fe9a3a830207ca8a7e19
NoSpread <nospread@pm.me>
2021-03-23 10:15:42 +0100

    update sql
    + changed encoding back to utf8
    + added foreign keys for uid
    + changed some vchar to length 50
    + uid cant be null
    + changed name to information

--- backend/mysqldb.sql
+++ backend/mysqldb.sql
@@ -1,87 +1,56 @@
--- MariaDB dump 10.19  Distrib 10.5.9-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: yapa
--- ------------------------------------------------------
--- Server version	10.5.9-MariaDB
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `keys`
---
 
-DROP TABLE IF EXISTS `keys`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `keys` (
+-- Dumping database structure for yapa
+CREATE DATABASE IF NOT EXISTS `yapa` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `yapa`;
+
+-- Dumping structure for table yapa.information
+CREATE TABLE IF NOT EXISTS `information` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `stocks` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `movement_type` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `workplaceCity` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `workplaceCode` int(11) DEFAULT NULL,
+  `workplaceStreet` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `jokequality` int(11) DEFAULT NULL,
+  `voice` int(11) DEFAULT NULL,
+  `residenceCity` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `residenceCode` int(11) DEFAULT NULL,
+  `residenceStreet` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `workstart` time DEFAULT NULL,
+  UNIQUE KEY `id` (`id`),
+  CONSTRAINT `uid_info` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table yapa.keys
+CREATE TABLE IF NOT EXISTS `keys` (
   `id` int(11) NOT NULL,
   `key` varchar(500) NOT NULL,
   `timestamp` timestamp NULL DEFAULT current_timestamp(),
   UNIQUE KEY `id` (`id`),
-  CONSTRAINT `uid` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `uid_key` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `keys`
---
+-- Data exporting was unselected.
 
-LOCK TABLES `keys` WRITE;
-/*!40000 ALTER TABLE `keys` DISABLE KEYS */;
-/*!40000 ALTER TABLE `keys` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `userinformation`
---
-
-DROP TABLE IF EXISTS `userinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userinformation` (
-  `id` int(11) DEFAULT NULL,
-  `fullname` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `stocks` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `movement_type` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `workplaceCity` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `workplaceCode` int(11) DEFAULT NULL,
-  `workplaceStreet` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `jokequality` int(11) DEFAULT NULL,
-  `voice` int(11) DEFAULT NULL,
-  `residenceCity` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `residenceCode` int(11) DEFAULT NULL,
-  `residenceStreet` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `workstart` time DEFAULT NULL,
-  KEY `UserId` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `userinformation`
---
-
-LOCK TABLES `userinformation` WRITE;
-/*!40000 ALTER TABLE `userinformation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userinformation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+-- Dumping structure for table yapa.users
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(500) NOT NULL,
@@ -89,24 +58,10 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `users`
---
+-- Data exporting was unselected.
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-03-23  9:14:34
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
